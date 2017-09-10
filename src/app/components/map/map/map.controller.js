ab294dd77b025928bc2704e98bb6e5a3f4283afb
Stefan Urech <stefan@surech.ch>
2017-09-10 17:26:01 +0200

    stu: Marker können auf der Karte ausgewählt werden und werden entsprechend anders dargestellt.

--- src/app/components/map/map/map.controller.js
+++ src/app/components/map/map/map.controller.js
@@ -1,4 +1,5 @@
 import markerIcon from './marker-icon.png';
+import selectedMarkerIcon from './selected-marker-icon.png';
 import markerIconShadow from './marker-shadow.png';
 
 class MapController {
@@ -11,11 +12,17 @@ class MapController {
         this.messagesService = messagesService;
 
         // Angezeigter Bereich der Karte
-        this.mapBounds = [];
+        this.mapBounds = {
+            "northEast": {"lat": 48.50204750525715, "lng": 11.722412109375},
+            "southWest": {"lat": 44.88701247981298, "lng": 4.691162109375}
+        };
         this.mapCenter = {};
 
         // Marker auf der Karte
         this.markers = [];
+
+        // Aktuell ausgewählter Marker
+        this.selectedMarker = null;
 
         this.message = "leer";
 
@@ -28,12 +35,27 @@ class MapController {
             shadowSize: [41, 41]
         };
 
+        this.selectedMarkerIcon = {
+            iconUrl: selectedMarkerIcon,
+            shadowUrl: markerIconShadow,
+            iconSize: [25, 41],
+            iconAnchor: [12, 41],
+            popupAnchor: [1, -34],
+            shadowSize: [41, 41]
+        };
+
         // ID der anzuzeigenden Map auslesen. Diese wird als Teil der URL übergeben
         this.currentMapId = this.$stateParams.mapId;
 
         this.$scope.$watch('$ctrl.mapBounds', (newValue, oldValue) => {this.boundChanged(newValue, oldValue)});
+        this.$scope.$on('leafletDirectiveMarker.click', (event, args) => {this.markerClicked(event, args)});
     }
 
+    /**
+     * Wird aufgerufen, wenn sich der angezeigte Bereich der Karte geändert hat.
+     * @param newValue Neuer, aktueller Ausschnitt
+     * @param oldValue Gewählter Ausschnitt vor der Änderung
+     */
     boundChanged(newValue, oldValue) {
         // Ohne Wert können wir nichts laden
         if(!newValue || newValue.length < 1){
@@ -45,15 +67,93 @@ class MapController {
             (response) => {
                 this.message = response;
                 var pointData = response._embedded.pointData;
-                var pointMarkers = [];
-                for(var i = 0; i < pointData.length; i++) {
-                    pointMarkers.push(this.createMarker(pointData[i]));
-                }
-                this.markers = pointMarkers;
+                this.displayMarkers(pointData);
             },
             (error) => {
                 this.messagesService.errorMessage("Punkte konnte nicht geladen werden: " + error, false);
             });
+    }
+
+    /**
+     * Wird aufgerufen, wenn ein Marker angeklickt wurde
+     */
+    markerClicked(event, args){
+        // Marker und Punkt auslesen
+        var marker = args.model;
+        var pointData = marker.pointData;
+
+        // Wichtig: Der Marker wird zwar als Argument übergeben. Dieser ist aber nur eine Kopie und alle Änderungen
+        // daran haben keine Auswirkungen auf die Karte. Deshalb suchen wir hier den "richtigen" Marker.
+        var trueMarker = this.findMarkerForPoint(pointData);
+
+        this.selectMarker(trueMarker, pointData);
+    }
+
+    /**
+     * Sucht einen Marker für einen bestimmten Punkt.
+     * @param point Punkt, dessen Marker gefunden werden soll
+     * @returns Gefundener Marker. <code>null</code>, wenn zum gegebenen Punkt kein Marker vorhanden ist.
+     */
+    findMarkerForPoint(point){
+        for(var i = 0; i < this.markers.length; i++){
+            var marker = this.markers[i];
+            var markerPoint = marker.pointData;
+            if(markerPoint.publicId === point.publicId){
+                return marker;
+            }
+        }
+
+        // Wenn wir hier sind, haben wir nichts gefunden
+        return null;
+    }
+
+    /**
+     * Zeigt die geladenen Punkte auf der Karte an
+     * @param pointData Liste mit den anzuzeigenden Punkten
+     */
+    displayMarkers(pointData){
+        var pointMarkers = [];
+        for(var i = 0; i < pointData.length; i++) {
+            var point = pointData[i];
+
+            // Den selektieren Punkt erstellen wir nicht neu, sondern übernehmen ihn am schluss
+            if(this.selectedMarker && point.publicId === this.selectedMarker.pointData.publicId){
+                continue;
+            }
+
+            // Marker erstellen und hinzufügen
+            pointMarkers.push(this.createMarker(point));
+        }
+
+        // Den selektierten Marker hinzufügen
+        if(this.selectedMarker){
+            pointMarkers.push(this.selectedMarker);
+        }
+
+        this.markers = pointMarkers;
+    }
+
+    /**
+     * Wählt einen Marker auf der Karte aus
+     * @param marker Auszuwählender Marker
+     * @param point Punkt hinter dem Marker
+     */
+    selectMarker(marker, point){
+        // Ist der Marker bereits selektiert?
+        if(this.selectedMarker && this.selectedMarker.pointData.publicId === point.publicId){
+            return;
+        }
+
+        // Aktueller Marker deselektieren
+        if(this.selectedMarker){
+            this.selectedMarker.icon = this.markerIcon;
+            this.selectedMarker.focus = false;
+        }
+
+        // Neuer Marker selektieren
+        this.selectedMarker = marker;
+        this.selectedMarker.icon = this.selectedMarkerIcon;
+        this.selectedMarker.focus = true;
     }
 
     /**
@@ -64,8 +164,11 @@ class MapController {
         var marker = {
             lat: pointData.location.latitude,
             lng: pointData.location.longitude,
-            "message": "PublicID: " + pointData.publicId,
-            "icon": this.markerIcon
+            // "message": "PublicID: " + pointData.publicId,
+            icon: this.markerIcon,
+            draggable: false,
+            focus: false,
+            "pointData": pointData
         };
         return marker;
     }
